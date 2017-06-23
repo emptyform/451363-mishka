5de1af1976c5e326803e103e6e25b0ff414aec4d
murci737 <firzen737@gmail.com>
2017-06-23 22:37:37 +0400

    Добавлены изменения из задания №21 (правки перед отправкой на финальную проверку)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 //menu
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove('main-nav--nojs');
-navToggle.classList.remove('main-nav__toggle--nojs');
+navMain.classList.remove("main-nav--nojs");
+navToggle.classList.remove("main-nav__toggle--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navToggle.classList.remove('main-nav__toggle--closed');
-    navToggle.classList.add('main-nav__toggle--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    navToggle.classList.remove("main-nav__toggle--closed");
+    navToggle.classList.add("main-nav__toggle--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navToggle.classList.add('main-nav__toggle--closed');
-    navToggle.classList.remove('main-nav__toggle--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+    navToggle.classList.add("main-nav__toggle--closed");
+    navToggle.classList.remove("main-nav__toggle--opened");
   }
 });
 
@@ -58,8 +58,8 @@ ymaps.ready(function () {
             hintContent: "Mishka",
             balloonContent: "Mishka Shop"
         }, {
-            iconLayout: 'default#image',
-            iconImageHref: 'img/icon-map-pin.svg',
+            iconLayout: "default#image",
+            iconImageHref: "img/icon-map-pin.svg",
             iconImageSize: [67, 100],
             iconImageOffset: [-40, -100]
         });
